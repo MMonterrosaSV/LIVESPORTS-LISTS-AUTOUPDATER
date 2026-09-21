b4d84481a4f9aa5af7888ef4a1eec7d408bba08f
MMonterrosaSV <MMonterrosaSV@gmail.com>
2026-09-21 16:58:26 +0200

    Update updater.py

--- scripts/updater.py
+++ scripts/updater.py
@@ -29,10 +29,6 @@ PLAYLISTS = [
         "filename": "FCTV33-FOOTBALL.m3u",
     },
     {
-        "url": "https://tvnow247-playlist.mmonterrosa970.workers.dev/playlist.m3u",
-        "filename": "TVNOW247-CHANNELS.m3u",
-    },
-    {
         "url": "https://capitanstrims.mmonterrosa970.workers.dev/?proxy=https://capitanstrims.st/&format_m3u",
         "filename": "CAPITANSTRIMS-FUTBOL.m3u",
     },
@@ -40,11 +36,10 @@ PLAYLISTS = [
         "url": "https://roxiestreams.mmonterrosa970.workers.dev/?url=https://roxiestreams.info/soccer&format=m3u8",
         "filename": "ROXIESTREAMS-SOCCER.m3u",
     },
-    # Add more like this (copy the block, change url + filename):
-    # {
-    #     "url": "https://thetvapp.mmonterrosa970.workers.dev/?url=https://thetvapp.plus/watch/cfb-streams&format=m3u",
-    #     "filename": "THETVAPP-CFB.M3U",
-    # },
+    # OFFLINE PLAYLISTS
+    # https://tvnow247-playlist.mmonterrosa970.workers.dev/playlist.m3u
+    #
+    #
 ]
 
 # scripts/updater.py → parent = scripts/ → parent.parent = repo root
